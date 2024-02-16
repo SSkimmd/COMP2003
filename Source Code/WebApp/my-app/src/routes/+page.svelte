79a53105ccc5b254e52cfadc794853f1bbb40401
SGCheney <91668480+SGCheney@users.noreply.github.com>
2024-02-16 02:43:56 +0000

    Basic login page structure and design

--- Source Code/WebApp/my-app/src/routes/+page.svelte
+++ Source Code/WebApp/my-app/src/routes/+page.svelte
@@ -19,19 +19,53 @@
     }
 </script>
 
-<div id="register">
-    <input bind:value={username} placeholder="Enter Username..."/>
-    <button on:click={registerPressed}>Register</button>
+<div id="fieldbox">
+    <div id="fieldbox-content">
+        <div id = "heading">
+            <h2>Login/Register User</h2>
+        </div>
+        <div id="login">
+            <input bind:value={username} placeholder="Enter Username..."/>
+            <button on:click={loginPressed}>Login</button>
+        </div>
+        
+        <div id="register">
+            <input bind:value={username} placeholder="Enter Username..."/>
+            <button on:click={registerPressed}>Register</button>
+        </div>
+    </div>
 </div>
 
-<div id="login">
-    <input bind:value={username} placeholder="Enter Username..."/>
-    <button on:click={loginPressed}>Login</button>
-</div>
+
 
 <style>
+    #fieldbox {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 25vh;
+        height: 50vh;
+        background-color: #EDEDED;
+        border-radius: 8px;
+        font-family: 'Franklin Gothic Light';
+    }
+    #fieldbox-content{
+        text-align: center;
+    }
+    #heading {
+        width: 100%;
+        height: 100%;
+        text-align: center;
+    }
     #login {
         width: 100%;
         height: 100%;
+        text-align: center;
+    }
+    #register {
+        width: 100%;
+        height: 100%;
+        text-align: center;
     }
 </style>
