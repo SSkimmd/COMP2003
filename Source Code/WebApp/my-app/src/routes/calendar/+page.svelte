059f1bc5eb89206de24e4d82ef533fefb2fb851d
explonewolf <73245276+explonewolf@users.noreply.github.com>
2024-02-23 23:20:38 +0000

    fetch and process calendar

--- Source Code/WebApp/my-app/src/routes/calendar/+page.svelte
+++ Source Code/WebApp/my-app/src/routes/calendar/+page.svelte
@@ -1,30 +1,74 @@
 <script>
-  import Navigation from "../../components/navigation.svelte";
+  import { onMount } from "svelte";
   import ClarityCalendarLine from '~icons/clarity/calendar-line';
   import IonReturnDownBackSharp from '~icons/ion/return-down-back-sharp';
   import { goto } from '$app/navigation';
+  import ical from 'node-ical'
+
+  let events = [];
+
+ 
+  async function fetchICal() {
+    const url = 'https://corsproxy.io/?' + 'https://calendar.google.com/calendar/ical/plymgroup28%40gmail.com/private-200aa8ee58a7ff3a5047b75d4392458d/basic.ics';
+    const response = await fetch(url);
+    const data = await response.text();
+    //console.log(data);
+    parseICal(data);
+}
+fetchICal();
+
+  function parseICal(data) {
+    const lines = data.split('\n');
+    let event = {};
+    for (let line of lines) {
+      if (line.startsWith('BEGIN:VEVENT')) {
+        event = {};
+      } else if (line.startsWith('DTSTART')) {
+        event.start = line.substring(8);
+      } else if (line.startsWith('DTEND')) {
+        event.end = line.substring(6);
+      } else if (line.startsWith('SUMMARY')) {
+        event.summary = line.substring(8);
+      } else if (line.startsWith('LOCATION')) {
+        event.location = line.substring(9);
+      } else if (line.startsWith('DESCRIPTION')) {
+        event.description = line.substring(12);
+      } else if (line.startsWith('END:VEVENT')) {
+        events.push(event);
+      }
+      console.log(event)
+    }
+  }
 
   function backPressed() {
-    goto('/home');
+    goto('/home')
   }
 </script>
 
 <div style='margin-top: 15vh;'>
-  <Navigation/>
-</div>
+  <div id="device">
+    <button on:click={backPressed} style='position:absolute; width: 40px; 
+    height: 40px; margin-top: 20px; margin-left: 20px; 
+    background-color: transparent; border: none; cursor: pointer;'>
+      <IonReturnDownBackSharp style='width: 30px; height: 30px;'/>
+    </button>
+    
+    <ClarityCalendarLine style='position: absolute; margin-top: 20px; margin-left: 70px; width: 30px; height: 30px;'/>
+    <p style='margin-left: 120px; margin-top: 25px;'>Calendar</p>
 
-<div id="device">
-  <button on:click={backPressed} style='position:absolute; width: 40px; 
-  height: 40px; margin-top: 20px; margin-left: 20px; 
-  background-color: transparent; border: none; cursor: pointer;'>
-    <IonReturnDownBackSharp style='width: 30px; height: 30px;'/>
-  </button>
-  
-  <ClarityCalendarLine style='position: absolute; margin-top: 20px; margin-left: 70px; width: 30px; height: 30px;'/>
-  <p style='margin-left: 120px; margin-top: 25px;'>Calendar</p>
-
-  <div id="device-content">
-    <iframe src="https://calendar.google.com/calendar/embed?src=plymgroup28%40gmail.com&ctz=Europe%2FLondon&key=AIzaSyDdCZh-NSkq5Vxamy3vez7nCUcEyY9-Ii0" style="border:solid 1px #777; width: 800px; height: 600px;" frameborder="0" scrolling="no"></iframe>
+    <div id="device-content">
+      <ul>
+        {#each events as event}
+          <li>
+            <strong>{event.summary}</strong><br>
+            {event.location}<br>
+            Start: {event.start}<br>
+            End: {event.end}<br>
+            Description: {event.description}
+          </li>
+        {/each}
+      </ul>
+    </div>
   </div>
 </div>
 
@@ -42,6 +86,7 @@
     height: 550px;
     background-color: #EDEDED;
     border-radius: 8px;
+
     font-family: 'Franklin Gothic Light';
   }
 </style>
